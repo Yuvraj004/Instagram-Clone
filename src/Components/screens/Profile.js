267d973008f9042776713b4db8e35aaa7279ea8c
Yuvraj Chaturvedi <96003202+Yuvraj004@users.noreply.github.com>
2022-10-23 01:08:51 +0530

    basic profile page

--- src/Components/screens/Profile.js
+++ src/Components/screens/Profile.js
@@ -1,16 +1,41 @@
-import React from 'react'
+/* eslint-disable jsx-a11y/alt-text */
+import React from "react";
 
-const Profile=()=> {
+const Profile = () => {
   return (
-
-    <div>
-      <div>
+    <div className="profile" >
+      <div className="p-section">
         <div>
-          <img style={{width:"160px",height:"160px",borderRadius:"80px"}}/>
+          <img className="dp"
+          style={{ }}
+          src="https://images.unsplash.com/photo-1552058544-f2b08422138a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8cGVyc29ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+          />
+        </div>
+        <div>
+          <h4>Craig Daniel</h4>
+          <div style={{ display: "flex", justifyContent: "space-around",width:"108%" }}>
+            <h5>40 posts</h5>
+            <h5>40 followers</h5>
+            <h5>40 following</h5>
+          </div>
         </div>
       </div>
+      <div className="gallery">
+        <img
+          className="item"
+          src="https://images.unsplash.com/photo-1552058544-f2b08422138a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8cGVyc29ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+        />
+        <img
+          className="item"
+          src="https://images.unsplash.com/photo-1552058544-f2b08422138a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8cGVyc29ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+        />
+        <img
+          className="item"
+          src="https://images.unsplash.com/photo-1552058544-f2b08422138a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8cGVyc29ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
