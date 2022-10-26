2d2a97ce446fff4da124d2bac48ffdb68de0f2ca
Yuvraj Chaturvedi <96003202+Yuvraj004@users.noreply.github.com>
2022-10-26 12:49:56 +0530

    profile page

--- src/Components/screens/Profile.js
+++ src/Components/screens/Profile.js
@@ -7,7 +7,6 @@ const Profile = () => {
       <div className="p-section">
         <div>
           <img className="dp"
-          style={{ }}
           src="https://images.unsplash.com/photo-1552058544-f2b08422138a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8cGVyc29ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
           />
         </div>
