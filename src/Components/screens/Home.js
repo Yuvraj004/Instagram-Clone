6bc3fb77e018be705463087bec5accec8003d6fc
Yuvraj Chaturvedi <96003202+Yuvraj004@users.noreply.github.com>
2022-10-26 13:12:40 +0530

    Home page

--- src/Components/screens/Home.js
+++ src/Components/screens/Home.js
@@ -2,7 +2,57 @@ import React from 'react'
 
 const Home=()=> {
   return (
-    <div><h1>Home</h1></div>
+    <div className='home'>
+      <div className='card home-card'>
+        <h2>Ramesh</h2>
+        <div className="card-image">
+          <img src='https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8d2FsbHBhcGVyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60' alt='...'/>
+        </div>
+        <div className="card-content">
+          <i className="material-icons" style={{color:"red"}}>favorite</i>
+          <h6>Title</h6>
+          <p>This is amazing post</p>
+          <input type="text" placeholder="Add a text"/>
+        </div>
+      </div>
+      <div className='card home-card'>
+        <h2>Ramesh</h2>
+        <div className="card-image">
+          <img src='https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8d2FsbHBhcGVyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60' alt='...'/>
+        </div>
+        <div className="card-content">
+          <i className="material-icons" style={{color:"red"}}>favorite</i>
+          <h6>Title</h6>
+          <p>This is amazing post</p>
+          <input type="text" placeholder="Add a text"/>
+        </div>
+      </div>
+      <div className='card home-card'>
+        <h2>Ramesh</h2>
+        <div className="card-image">
+          <img src='https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8d2FsbHBhcGVyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60' alt='...'/>
+        </div>
+        <div className="card-content">
+          <i className="material-icons" style={{color:"red"}}>favorite</i>
+          <h6>Title</h6>
+          <p>This is amazing post</p>
+          <input type="text" placeholder="Add a text"/>
+        </div>
+      </div>
+      <div className='card home-card'>
+        <h2>Ramesh</h2>
+        <div className="card-image">
+          <img src='https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8d2FsbHBhcGVyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60' alt='...'/>
+        </div>
+        <div className="card-content">
+          <i className="material-icons" style={{color:"red"}}>favorite</i>
+          <h6>Title</h6>
+          <p>This is amazing post</p>
+          <input type="text" placeholder="Add a text"/>
+        </div>
+      </div>
+
+    </div>
   )
 }
 
