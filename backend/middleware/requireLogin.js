d868a9a1d15df2a0bb45a12beba5e68fbadea79f
Yuvraj Chaturvedi <96003202+Yuvraj004@users.noreply.github.com>
2023-03-04 16:50:46 +0530

    to scheckk the problems

--- backend/middleware/requireLogin.js
+++ backend/middleware/requireLogin.js
@@ -4,9 +4,9 @@ require("dotenv").config({ path: "../.env" });
 const JWT_KEY = "" + process.env.JWT_KEY;
 //accessing the user model
 const User = require("../models/user");
-
 module.exports = async (req, res, next) => {
     const auth = await req.headers.authorization;
+    console.log(auth);
     //auth === Bearer ewedfanwoir
     if (!auth) {
         return res.status(401).json({ error: "You must be auth logged in" });
