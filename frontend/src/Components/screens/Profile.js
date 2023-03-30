f62d51a898d1ef456951cdcb78ee9951dc8ee11d
Yuvraj Chaturvedi <96003202+Yuvraj004@users.noreply.github.com>
2023-03-30 13:59:54 +0530

    re5

--- frontend/src/Components/screens/Profile.js
+++ frontend/src/Components/screens/Profile.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useCallback, useState, useContext } from "react";
+import { Link } from "react-router-dom";
 import { UserContext } from "../../App";
 require("dotenv").config({ path: "./.env" });
 const Profile = () => {
   const [mypics, setPics] = useState([]);
-  const { state,dispatch } = useContext(UserContext);
+  const { state, dispatch } = useContext(UserContext);
   // const [image, setImage] = useState("")
   const [url, setUrl] = useState(undefined)
   const logResult = useCallback(() => {
@@ -26,11 +27,29 @@ const Profile = () => {
           <img className="dp"
             src={state ? state.pic : "loading...!"}
           />
-          
+
           <div className="button probtn">
-            <span><a href="/updatepfp">Update Profile Photo</a></span>
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+              Update Profile Photo
+            </button>
           </div>
-          
+        </div>
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div class="modal-body">
+                ...
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+              </div>
+            </div>
+          </div>
         </div>
         <div>
           <h4>{state ? state.name : "loading"}</h4>
@@ -43,11 +62,11 @@ const Profile = () => {
         </div>
       </div>
       <div className="gallery" style={{ "display": "flex" }}>
-        {(mypics.length==0)?"NO POSTS YET ":mypics.map(item => {
+        {(mypics.length == 0) ? "NO POSTS YET " : mypics.map(item => {
           return (
             <>
               <h2>Your Posts</h2>
-              <div style={{ "display": "grid", "columnCount": 3,"rowCount":"3", "gap": "50px 20px", "gridTemplateColumns": "auto auto" }}>
+              <div style={{ "display": "grid", "columnCount": 3, "rowCount": "3", "gap": "50px 20px", "gridTemplateColumns": "auto auto" }}>
                 <div key={item._id} style={{ "display": "flex" }}>
                   {/* <p>{item.title}</p> */}
                   <img
